b23164067fd85c5a10d1b09086506c58a2113984
robin <robin.oqvist@yh.nackademin.se>
2024-11-21 11:19:38 +0100

    Player GUI tillagd

---
+++ src/PlayerGUI.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+
+public class PlayerGUI extends JFrame {
+        private String name;
+        private int score;
+
+        private JFrame frame;
+        private JLabel nameLabel, scoreLabel;
+        private JButton button;
+        private JButton resetButton;
+
+        public PlayerGUI() {
+            this.name = JOptionPane.showInputDialog("Enter your name");
+            this.score = 0;
+
+
+            frame = new JFrame("Player");
+            nameLabel = new JLabel("Player" + name);
+            scoreLabel = new JLabel("Score: " + score);
+            button = new JButton("Counter");
+            resetButton = new JButton("Reset");
+
+            JPanel panel = new JPanel();
+            panel.add(nameLabel);
+            panel.add(scoreLabel);
+            panel.add(button);
+            panel.add(resetButton);
+
+            frame.add(panel);
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+            frame.setSize(500, 500);
+
+            button.addActionListener(e -> {
+                score++;
+                scoreLabel.setText("Score: " + score);
+            });
+            resetButton.addActionListener(e -> {
+                score = 0;
+                scoreLabel.setText("Score: " + score);
+
+            });
+            JOptionPane.showMessageDialog(frame, "Welcome " + name);
+        }
+        public static void main(String[] args) {
+            new PlayerGUI();
+        }
+    }
+
